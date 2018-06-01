430b17c9132693aafef54afd1a4bec33fc545792
rdparihar <rdparihar@yahoo.com>
2018-06-01 19:31:44 +0530

    sesson borrowd books

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
+from datetime import date
 
 # Create your models here.
 
@@ -62,6 +64,7 @@ class BookInstance(models.Model):
     book = models.ForeignKey('Book', on_delete=models.SET_NULL, null=True) 
     imprint = models.CharField(max_length=200)
     due_back = models.DateField(null=True, blank=True)
+    borrower = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
 
     LOAN_STATUS = (
         ('m', 'Maintenance'),
@@ -70,6 +73,11 @@ class BookInstance(models.Model):
         ('r', 'Reserved'),
     )
 
+    @property
+    def is_overdue(self):
+        if self.due_back and date.today() > self.due_back:
+            return True
+        return False
     status = models.CharField(max_length=1, choices=LOAN_STATUS, blank=True, default='m', help_text='Book availability')
 
     class Meta:
@@ -81,6 +89,8 @@ class BookInstance(models.Model):
         String for representing the Model object
         """
         return '{0} ({1})'.format(self.id,self.book.title)
+
+   
 
 class Author(models.Model):
     """
